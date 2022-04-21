8d4fb1ca39324dde489a6a0fe71391329e5359aa
Steven Balalis <stevenbalalis@Stevens-MacBook-Pro.local>
2022-04-21 14:20:30 -0400

    Update index.js
    Now the count actually renders

--- index.js
+++ index.js
@@ -12,9 +12,3 @@ if (visitCount) {
 }
 counterContainer.innerHTML = visitCount;
 
-// Adding onClick event listener
-resetButton.addEventListener("click", () => {
-  visitCount = 1:
-  localStorage.setItem("page_view", 1);
-  counterContainer.innerHTML = visitCount;
-}); 
